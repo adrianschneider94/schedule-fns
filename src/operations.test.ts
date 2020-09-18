6a0fe99878c4edf04bac4c64cd61fc36072d0784
adrianschneider94 <post@adrian-schneider.de>
2020-09-18 11:24:00 +0200

    Added joinSchedules

--- src/operations.test.ts
+++ src/operations.test.ts
@@ -1,5 +1,5 @@
 import {ScheduleFromIntervals} from "./schedules"
-import {invertSchedule} from "./operations"
+import {invertSchedule, joinSchedules} from "./operations"
 import {areIntervalsEqual} from "./functions"
 
 test('Invert simple schedule', () => {
@@ -49,5 +49,90 @@ test("Invert schedule with infinity", () => {
     let invertedSchedule = invertSchedule(schedule)
     let generator = invertedSchedule(0)
 
+    expect(generator.next()).toStrictEqual({value: undefined, done: true})
+})
+
+test('Join single schedule', () => {
+    let schedule = ScheduleFromIntervals([{start: 0, end: 1}, {start: 2, end: 3}])
+    let joinedSchedule = joinSchedules(schedule)
+    let generator = joinedSchedule(0)
+
+    let value = generator.next()
+    expect(value.done).toBe(false)
+    expect(areIntervalsEqual(value.value, {start: 0, end: 1})).toBe(true)
+
+    value = generator.next()
+    expect(value.done).toBe(false)
+    expect(areIntervalsEqual(value.value, {start: 2, end: 3})).toBe(true)
+
+    expect(generator.next()).toStrictEqual({value: undefined, done: true})
+})
+
+test('Join two simple schedules', () => {
+    let schedule1 = ScheduleFromIntervals([{start: 0, end: 10}, {start: 19, end: 30}])
+    let schedule2 = ScheduleFromIntervals([{start: 9, end: 20}])
+    let joinedSchedule = joinSchedules(schedule1, schedule2)
+
+    let generator = joinedSchedule(0)
+    let value = generator.next()
+    expect(value.done).toBe(false)
+    expect(areIntervalsEqual(value.value, {start: 0, end: 30})).toBe(true)
+    expect(generator.next()).toStrictEqual({value: undefined, done: true})
+})
+
+test('Join three simple schedules', () => {
+    let schedule1 = ScheduleFromIntervals([{start: 0, end: 1}, {start: 3, end: 4}])
+    let schedule2 = ScheduleFromIntervals([{start: 1, end: 2}, {start: 5, end: 6}])
+    let schedule3 = ScheduleFromIntervals([{start: 2, end: 3}, {start: 6, end: 7}])
+    let joinedSchedule = joinSchedules(schedule1, schedule2, schedule3)
+    let generator = joinedSchedule(-10)
+
+    let value = generator.next()
+    expect(value.done).toBe(false)
+    expect(areIntervalsEqual(value.value, {start: 0, end: 4})).toBe(true)
+
+    value = generator.next()
+    expect(value.done).toBe(false)
+    expect(areIntervalsEqual(value.value, {start: 5, end: 7})).toBe(true)
+
+    expect(generator.next()).toStrictEqual({value: undefined, done: true})
+})
+
+test('Join two schedules (with infinity)', () => {
+    let schedule1 = ScheduleFromIntervals([{start: 0, end: Infinity}])
+    let schedule2 = ScheduleFromIntervals([{start: 1, end: 2}])
+    let joinedSchedule = joinSchedules(schedule1, schedule2)
+    let generator = joinedSchedule(-10)
+
+    let value = generator.next()
+    expect(value.done).toBe(false)
+    expect(areIntervalsEqual(value.value, {start: 0, end: Infinity})).toBe(true)
+
+    expect(generator.next()).toStrictEqual({value: undefined, done: true})
+})
+
+test('Join two schedules (one long-running)', () => {
+    let schedule1 = ScheduleFromIntervals([{start: 0, end: 1000}])
+    let schedule2 = ScheduleFromIntervals(
+        [
+            {start: 1, end: 2},
+            {start: 3, end: 4},
+            {start: 5, end: 6},
+            {start: 7, end: 8}
+        ]
+    )
+    let joinedSchedule = joinSchedules(schedule1, schedule2)
+    let generator = joinedSchedule(-10)
+
+    let value = generator.next()
+    expect(value.done).toBe(false)
+    expect(areIntervalsEqual(value.value, {start: 0, end: 1000})).toBe(true)
+
+    expect(generator.next()).toStrictEqual({value: undefined, done: true})
+})
+
+test('Join no schedules', () => {
+    let joinedSchedule = joinSchedules()
+    let generator = joinedSchedule(0)
     expect(generator.next()).toStrictEqual({value: undefined, done: true})
 })
--- src/operations.ts
+++ src/operations.ts
@@ -1,5 +1,6 @@
 import {DateInfinity, Schedule} from "./index"
-import {isEqual} from "date-fns"
+import {compareAsc, isEqual} from "date-fns"
+import {areIntervalsConnected, isEmpty, joinIntervals} from "./functions"
 
 export function invertSchedule(schedule: Schedule): Schedule {
     return function* (startDate) {
@@ -16,6 +17,36 @@ export function invertSchedule(schedule: Schedule): Schedule {
         }
         if (last && !isEqual(last.end, DateInfinity)) {
             yield {start: last.end, end: DateInfinity}
+        }
+    }
+}
+
+export function joinSchedules(...schedules: Array<Schedule>): Schedule {
+    return function* (startDate) {
+        if (isEmpty(schedules)) {
+            return
+        }
+
+        let generators = schedules.map(schedule => schedule(startDate))
+        let currentEntries = generators.map(generator => generator.next())
+        let currentInterval: Interval = [...currentEntries].filter(x => x?.value !== undefined).sort((a, b) => compareAsc(a.value.start, b.value.start))[0].value
+
+        while (true) {
+            let updated = false
+            for (const [i, entry] of currentEntries.entries()) {
+                if (entry?.value && areIntervalsConnected(currentInterval, entry.value)) {
+                    currentInterval = joinIntervals(currentInterval, entry.value)
+                    currentEntries[i] = generators[i].next()
+                    updated = true
+                }
+            }
+            if (!updated) {
+                yield currentInterval
+                if (currentEntries.every(x => x.done) || isEqual(currentInterval.end, Infinity)) {
+                    return
+                }
+                currentInterval = [...currentEntries].filter(x => x?.value !== undefined).sort((a, b) => compareAsc(a.value.start, b.value.start))[0].value
+            }
         }
     }
 }
