26336d7a8820374e5ffcc15df4a6e4d8d9d44441
adrianschneider94 <post@adrian-schneider.de>
2020-09-18 14:26:10 +0200

    Added RegularSchedule

--- src/schedules.test.ts
+++ src/schedules.test.ts
@@ -1,4 +1,5 @@
-import {ScheduleFromIntervals} from "./schedules"
+import {RegularSchedule, ScheduleFromIntervals} from "./schedules"
+import {areIntervalsEqual} from "./functions"
 
 test('Basic schedule from intervals', () => {
     let intervals = [{start: 0, end: 10}, {start: 20, end: 30}]
@@ -26,4 +27,47 @@ test('Schedule from intervals with startDate after all intervals', () => {
     let schedule = ScheduleFromIntervals({start: 0, end: 10}, {start: 20, end: 30})
     let generator = schedule(35)
     expect(generator.next()).toStrictEqual({value: undefined, done: true})
+})
+
+test('RegularSchedule', () => {
+    let schedule = RegularSchedule(0, {seconds: 1}, {seconds: 2})
+    let generator = schedule(0)
+
+    let value = generator.next()
+    expect(value.done).toBeFalsy()
+    expect(areIntervalsEqual(value.value, {start: 0, end: 1000})).toBeTruthy()
+
+    value = generator.next()
+    expect(value.done).toBeFalsy()
+    expect(areIntervalsEqual(value.value, {start: 2000, end: 3000})).toBeTruthy()
+
+    value = generator.next()
+    expect(value.done).toBeFalsy()
+    expect(areIntervalsEqual(value.value, {start: 4000, end: 5000})).toBeTruthy()
+})
+
+test('RegularSchedule with offset start 1', () => {
+    let schedule = RegularSchedule(0, {seconds: 1}, {seconds: 2})
+    let generator = schedule(5500)
+
+    let value = generator.next()
+    expect(value.done).toBeFalsy()
+    expect(areIntervalsEqual(value.value, {start: 6000, end: 7000})).toBeTruthy()
+
+    value = generator.next()
+    expect(value.done).toBeFalsy()
+    expect(areIntervalsEqual(value.value, {start: 8000, end: 9000})).toBeTruthy()
+})
+
+test('RegularSchedule with offset start 2', () => {
+    let schedule = RegularSchedule(0, {seconds: 1}, {seconds: 2})
+    let generator = schedule(500)
+
+    let value = generator.next()
+    expect(value.done).toBeFalsy()
+    expect(areIntervalsEqual(value.value, {start: 500, end: 1000})).toBeTruthy()
+
+    value = generator.next()
+    expect(value.done).toBeFalsy()
+    expect(areIntervalsEqual(value.value, {start: 2000, end: 3000})).toBeTruthy()
 })
--- src/schedules.ts
+++ src/schedules.ts
@@ -1,6 +1,6 @@
 import {Schedule} from "./index"
-import {mergeIntervals} from "./functions"
-import {isWithinInterval} from "date-fns"
+import {durationToMilliseconds, mergeIntervals, multiplyDuration} from "./functions"
+import {add, differenceInMilliseconds, isWithinInterval} from "date-fns"
 
 export function ScheduleFromIntervals(...intervals: Array<Interval>): Schedule {
     return function* (startDate) {
@@ -11,6 +11,35 @@ export function ScheduleFromIntervals(...intervals: Array<Interval>): Schedule {
             } else {
                 yield interval
             }
+        }
+    }
+}
+
+export function RegularSchedule(firstOccurence: Date | number, duration: Duration, period: Duration): Schedule {
+    return function* (startDate) {
+        if (firstOccurence <= startDate) {
+            let fullPeriodsBeforeStartDate = Math.floor(differenceInMilliseconds(startDate, firstOccurence) / durationToMilliseconds(period))
+            firstOccurence = add(firstOccurence, multiplyDuration(period, fullPeriodsBeforeStartDate))
+            if (add(firstOccurence, duration) <= startDate) {
+                firstOccurence = add(firstOccurence, period)
+            }
+        }
+        let i = 0
+        while (true) {
+            let start = add(firstOccurence, multiplyDuration(period, i))
+            let interval = {
+                start: start,
+                end: add(start, duration)
+            }
+            if (isWithinInterval(startDate, interval)) {
+                yield {
+                    start: startDate,
+                    end: interval.end
+                }
+            } else {
+                yield interval
+            }
+            i++
         }
     }
 }
