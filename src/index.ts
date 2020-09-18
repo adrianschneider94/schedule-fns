0d593c4119a4d7a493cf029ffb374726fc9d1255
adrianschneider94 <post@adrian-schneider.de>
2020-09-18 13:35:11 +0200

    Export relevant functions and constants in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import {Interval} from "date-fns"
 
-export type Schedule = (startDate: Date | number) => IterableIterator<Interval>
 export const DateInfinity = 8640000000000000
+
+export type Schedule = (startDate: Date | number) => IterableIterator<Interval>
+export {ScheduleFromIntervals} from "./schedules"
+export {invertSchedule, joinSchedules, intersectSchedules, subtractSchedules, symmetricDifferenceOfSchedules} from "./operations"
